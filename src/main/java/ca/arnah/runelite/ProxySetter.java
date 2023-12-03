0a3dd16696ad8c62333cde71e9355c98532a06cc
Ian Davis <ianfights@gmail.com>
2023-12-02 22:13:45 -0500

    Configured all proxy methods to use the new JSON format

--- src/main/java/ca/arnah/runelite/ProxySetter.java
+++ src/main/java/ca/arnah/runelite/ProxySetter.java
@@ -1,28 +1,32 @@
 package ca.arnah.runelite;
 
+import org.json.JSONObject;
 public class ProxySetter {
-    public static void setProxyProperties(String[] proxy) {
-        if (proxy.length >= 2) {
-            System.setProperty("socksProxyHost", proxy[0]);
-            System.setProperty("socksProxyPort", proxy[1]);
-            System.setProperty("socksProxyVersion", "5"); // Set SOCKS5 version
+    public static void setProxyProperties(JSONObject proxy) {
+        // If there is no username then there will be no password so need to check the length
+        if (proxy.getString("username")!= "") {
+            System.out.println("Proxy has authentication, setting credentials");
+            System.setProperty("java.net.socks.username", proxy.getString("username"));
+            System.setProperty("java.net.socks.password", proxy.getString("password"));
+
         }
-        if (proxy.length >= 4) {
-            System.setProperty("java.net.socks.username", proxy[2]);
-            System.setProperty("java.net.socks.password", proxy[3]);
+        // The proxy always needs this so we set it
+            System.setProperty("socksProxyHost", proxy.getString("host"));
+            System.setProperty("socksProxyPort", proxy.getString("port"));
             System.setProperty("socksProxyVersion", "5"); // Set SOCKS5 version
-        }
+
     }
 
 
-    public static void setHttpProxyProperties(String[] proxy) {
-        if (proxy.length >= 2) {
-            System.setProperty("http.proxyHost", proxy[0]);
-            System.setProperty("http.proxyPort", proxy[1]);
+    public static void setHttpProxyProperties(JSONObject proxy) {
+        if (proxy.getString("username") != "") {
+            System.setProperty("http.proxyUser", proxy.getString("username"));
+            System.setProperty("http.proxyPassword", proxy.getString("password"));
+
         }
-        if (proxy.length >= 4) {
-            System.setProperty("http.proxyUser", proxy[2]);
-            System.setProperty("http.proxyPassword", proxy[3]);
-        }
+
+            System.setProperty("http.proxyHost", proxy.getString("host"));
+            System.setProperty("http.proxyPort", proxy.getString("port"));
+
     }
 }
